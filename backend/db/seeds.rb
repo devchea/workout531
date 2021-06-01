6497d00ee404c909de20509573d8213556cac4d2
devchea <rchea817@gmail.com>
2021-06-01 16:35:08 -0700

    "updates"

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+exercise_list = [
+    "inchworm",
+    "tuck jump",
+    "bear crawl",
+    "mountain climber",
+    "plyometric push-up",
+    "stair climbers",
+    "walkout",
+    "wall sit",
+    "plank",
+    "push ups"
+
+]
+
+exercise_list.each do |exercise|
+
+    Exercise.create(
+        name:"#{exercise}",
+        repetition:rand(8),
+        description:Faker::Food.description,
+        date:DateTime.new(2020,8,(rand(30)))
+    )
+    
+end
